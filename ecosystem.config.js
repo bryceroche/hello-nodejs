66541f5648deda8247a7e72618c574e7e5356793
Bryce Roche <bryceroche@bryces-imac.lan>
2017-08-23 13:26:12 -0700

    asb

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'hello-nodejs',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-149-95-169.us-west-2.compute.amazonaws.com',
+      key: '~/Desktop/hello-nodejs/ec2_expressv1.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/bryceroche/hello-nodejs',
+      path: '/home/ubuntu/server/hello-nodejs',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
