f941352cf92566d56a176934db478355b18cffd7
Bryce Roche <bryceroche@bryces-imac.lan>
2017-08-23 13:27:52 -0700

    setup file change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-54-149-95-169.us-west-2.compute.amazonaws.com',
-      key: '~/Desktop/hello-nodejs/ec2_expressv1.pem',
+      key: '~/Desktop/hello-nodejs/hello-nodejs/ec2_expressv1.pem',
       ref: 'origin/master',
       repo: 'https://github.com/bryceroche/hello-nodejs',
       path: '/home/ubuntu/server/hello-nodejs',
