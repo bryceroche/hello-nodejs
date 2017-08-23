58fd98c2e21d98c5a7ef39a46d849840822333f3
Bryce Roche <bryceroche@bryces-imac.lan>
2017-08-23 13:38:27 -0700

    new message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('another message .... from hello world exmample!')
+  res.send('finishing up tutorial #2 ... ah yeah!  ADRI FOR LIFE')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
