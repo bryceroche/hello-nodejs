513e502625bee45544c3e2ac52484469ab51369b
Bryce Roche <bryceroche@bryces-imac.lan>
2017-08-23 17:31:13 -0700

    change to index file

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('finishing up tutorial #2 ... ah yeah!  ADRI FOR LIFE')
-})
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
