e17bad89e8ca795c98b3a8a528ec9c8886960018
Bryce Roche <bryceroche@bryces-imac.lan>
2017-08-24 09:39:25 -0700

    added the routing v1

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
